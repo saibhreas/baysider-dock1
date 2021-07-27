b03a36fa162c5ef900d41fc154aa446eef521dee
siobhan <sr@saibhreas.com>
2021-07-27 10:11:15 -0400

    minor revsion to testing routes. added params.  nwo moving to router

--- server.js
+++ server.js
@@ -7,17 +7,25 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+//Get All
 app.get('/api/v1/bootcamps', (req, res) => {
   res.status(200).json({ success: true, msg:'Show all Bootcamps'});
 });
+//Get One
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg:` ${req.params.id}`});
+});
+//Create One
 app.post('/api/v1/bootcamps', (req, res) => {
   res.status(200).json({ success: true, msg:'Create new Bootcamp'});
 });
-app.put('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg:'Update single Bootcamp'});
+//Update one
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg:`Display Bootcamp ${req.params.id}`});
 });
-app.delete('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg:'Delete single Bootcamp'});
+//Delte One
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res.status(200).json({ success: true, msg:`Delete Bootcamp ${req.params.id}`});
 });
 
 const PORT = process.env.PORT || 5000;
