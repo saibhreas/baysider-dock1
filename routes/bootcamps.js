4605abe0648ca7a36ae4556db61f381443594595
siobhan <sr@saibhreas.com>
2021-07-28 12:02:19 -0400

    setting route folder systems.  Server proved with routes written in. now to time to move to express router system.

---
+++ routes/bootcamps.js
@@ -0,0 +1,25 @@
+const express = require ('expres');
+const router = express.Router();
+
+//Get All
+router.get('/', (req, res) => {
+  res.status(200).json({ success: true, msg:'Show all Bootcamps'});
+});
+//Get One
+router.get('/:id', (req, res) => {
+  res.status(200).json({ success: true, msg:` ${req.params.id}`});
+});
+//Create One
+router.post('/s', (req, res) => {
+  res.status(200).json({ success: true, msg:'Create new Bootcamp'});
+});
+//Update one
+router.put('/:id', (req, res) => {
+  res.status(200).json({ success: true, msg:`Display Bootcamp ${req.params.id}`});
+});
+//Delte One
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ success: true, msg:`Delete Bootcamp ${req.params.id}`});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,31 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
+//link to route files
+const bootcamps = require('./routes/bootcamps');
+
 //Load config file-- Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-//Get All
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg:'Show all Bootcamps'});
-});
-//Get One
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg:` ${req.params.id}`});
-});
-//Create One
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg:'Create new Bootcamp'});
-});
-//Update one
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg:`Display Bootcamp ${req.params.id}`});
-});
-//Delte One
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg:`Delete Bootcamp ${req.params.id}`});
-});
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+
 
 const PORT = process.env.PORT || 5000;
 
