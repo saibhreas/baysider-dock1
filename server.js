6672f8624b5cb440025bd16e8e297707d0447e78
siobhan <sr@saibhreas.com>
2021-07-30 15:41:53 -0400

    keep crashing with the create bootcamp. cannot see why

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 //Dev logging middleware
 //only want to run in dev mode
 if(process.env.NODE_ENV === 'development'){
