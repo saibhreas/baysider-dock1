52184810209a89a212530f153598b766141217d5
siobhan <sr@saibhreas.com>
2021-07-30 13:46:01 -0400

    added uhandled promise error: unhandledRejection. Used colors to accent if the eror hits

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require('morgan');
 const colors = require('colors');
+
 const connectDB = require('./config/db');
 
 //Load config file-- Load env vars
@@ -10,17 +11,9 @@ dotenv.config({ path: "./config/config.env" });
 // Connect to database
 connectDB();
 
-
-const mongoose = require("mongoose");
-
-
-
-//link to route files
+//Route files
 const bootcamps = require('./routes/bootcamps');
 
-
-//Load config file-- Load env vars
-dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
@@ -29,6 +22,7 @@ const app = express();
 if(process.env.NODE_ENV === 'development'){
   app.use(morgan('dev'))
 }
+
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
@@ -38,5 +32,9 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.Node_env} mode on port ${PORT}`.yellow.bold)
+  console.log(`Server running in ${process.env.Node_env} mode on port ${PORT}`.brightBlue.bold)
 );
+
+process.on('unhandledRejection', (err, promise) =>{
+  console.log(`Error:{err.message}`.bgWhite.brightRed.bold)
+})
