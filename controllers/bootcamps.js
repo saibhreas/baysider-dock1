b7ffb69f4d63b5d0679b08a60682bc277ed5feae
siobhan <sr@saibhreas.com>
2021-08-14 18:58:19 -0400

    sorting done moving onto pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -14,11 +14,11 @@ exports.getBootcamps = asyncHandler(async (req, res, next)=> {
   const reqQuery = { ...req.query};
 
   // Fields to Exclude using the word 'select'
-  const removeFields = ['select'];
+  const removeFields = ['select', 'sort'];
 
   // Loop over removeFields and delete them from reqQuery
   removeFields.forEach(param => delete reqQuery[param]);
-  console.log(reqQuery);
+  
 
   // Create query String
   let queryStr = JSON.stringify(reqQuery);
@@ -28,6 +28,20 @@ exports.getBootcamps = asyncHandler(async (req, res, next)=> {
   
   // Finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
+
+  // Select Fields
+  if(req.query.select){
+    const fields = req.query.select.split(',').join(' ');
+    query =query.select(fields);
+  }
+
+  // Sort
+  if(req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy);
+  }else{
+    query = quer.sort('-createdAt'); //default sort
+  }
 
   // Executing Query
   const bootcamps = await query;
